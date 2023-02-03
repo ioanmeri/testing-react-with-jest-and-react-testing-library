496e1f1523a8cf5cc86f6ead5c3ec02d8206e74b
Ioannis Merianos <ioannismeri@gmail.com>
2023-02-03 21:25:24 +0200

    add first test: checkbox disables button

--- 01-color-button-app/src/App.js
+++ 01-color-button-app/src/App.js
@@ -1,10 +1,21 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div>
-      <button style={{ backgroundColor: "red" }}>Change to blue</button>
+      <button
+        disabled={disabled}
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
+      <input type="checkbox" onChange={(e) => setDisabled(e.target.checked)} />
     </div>
   );
 }

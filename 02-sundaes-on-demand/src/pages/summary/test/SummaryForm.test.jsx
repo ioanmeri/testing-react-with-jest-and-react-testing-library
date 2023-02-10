75d124392130454262c74531ad7c970f8f9fa00e
Ioannis Merianos <ioannismeri@gmail.com>
2023-02-10 22:45:57 +0200

    add tests for popover, getByText and queryByText

--- 02-sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ 02-sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -33,10 +33,21 @@ test("Checkbox disables button on first click and enables on second click", asyn
 
 test("popover response to hover", async () => {
   const user = userEvent.setup();
+  render(<SummaryForm />);
 
   // popup starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
 
   // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
 
   // popover disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });

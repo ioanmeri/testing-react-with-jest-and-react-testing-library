3e4c52724f29b07b2c926a9c9129204fdc84ded5
Ioannis Merianos <ioannismeri@gmail.com>
2023-02-14 22:56:21 +0200

    react code for options

--- 02-sundaes-on-demand/src/pages/entry/Options.jsx
+++ 02-sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ScoopOption from "./ScoopOption";
+import Row from "react-bootstrap/Row";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
@@ -26,5 +27,5 @@ export default function Options({ optionType }) {
     />
   ));
 
-  return <div>{optionItems}</div>;
+  return <Row>{optionItems}</Row>;
 }

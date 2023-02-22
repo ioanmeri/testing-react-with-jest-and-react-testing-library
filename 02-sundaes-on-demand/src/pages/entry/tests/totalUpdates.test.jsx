46c30dba69871001917d1325335c5c42471b8b5d
Ioannis Merianos <ioannismeri@gmail.com>
2023-02-22 23:21:22 +0200

    creating custom render to wrap in ProviderBy Default

--- 02-sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
+++ 02-sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,11 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
-import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 import Options from "../Options";
 
 test("update scoop subtotal when scoops change", async () => {
   const user = userEvent.setup();
-  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+  render(<Options optionType="scoops" />);
 
   // make sure total starts out $0.00
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });

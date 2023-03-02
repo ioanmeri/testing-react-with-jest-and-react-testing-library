b92f9294a93b0f03139ee1707781947d7142ea72
Ioannis Merianos <ioannismeri@gmail.com>
2023-03-02 21:33:20 +0200

    final exam solution

--- 02-sundaes-on-demand/src/tests/orderPhase.test.jsx
+++ 02-sundaes-on-demand/src/tests/orderPhase.test.jsx
@@ -3,14 +3,97 @@ import userEvent from "@testing-library/user-event";
 
 import App from "../App";
 
-test("order phases for happy path", () => {
+test("order phases for happy path", async () => {
+  const user = userEvent.setup();
   // render app
+  // Don't need to wrap in provider already wrapped!
+
+  // descructure `unmount` from return value to use at the end of the test
+  const { unmount } = render(<App />);
+
   // add ice cream scoops and toppings
-  // find and click order button
-  // check summary information based on order
-  // accept terms and conditions and click button to confirm order
-  // confirm order number on confirmation page
-  // click new order button on confirmation page
-  // check that scoops and toppings subtotals have been reset
-  // do we need to await anything to avoid test errors?
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+
+  const chocolateInput = screen.getByRole("spinbutton", { name: "Chocolate" });
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesCheckbox);
+
+  // find and click order summary button
+  const orderSummaryButton = screen.getByRole("button", {
+    name: /order sundae/i,
+  });
+  await user.click(orderSummaryButton);
+
+  // check summary subtotals
+  const summaryHeading = screen.getByRole("heading", { name: "Order Summary" });
+  expect(summaryHeading).toBeInTheDocument();
+
+  const toppingsHeading = screen.getByRole("heading", {
+    name: "Toppings: $1.50",
+  });
+  expect(toppingsHeading).toBeInTheDocument();
+
+  // check summary option items
+  expect(screen.getByText("1 Vanilla")).toBeInTheDocument();
+  expect(screen.getByText("2 Chocolate")).toBeInTheDocument();
+  expect(screen.getByText("Cherries")).toBeInTheDocument();
+
+  // alternatively...
+  // const optionItems = screen.getAllByRole("listitem");
+  // const optionItemsText = optionItems.map((item) => item.textContent);
+  // expect(optionItemsText).toEqual(["1 Vanilla", "2 Chocolate", "Cherries"]);
+
+  // accept terms and click button
+  const tcCheckbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  await user.click(tcCheckbox);
+
+  const confirmOrderButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+  await user.click(confirmOrderButton);
+
+  // Expect "loading" to show
+  const loading = screen.getByText(/loading/i);
+  expect(loading).toBeInTheDocument();
+
+  // check confirmation page text
+  // this one is async because there is a POST request to server in between summary
+  // and confirmation pages
+
+  const thankYouHeader = await screen.findByRole("heading", {
+    name: /thank you/i,
+  });
+  expect(thankYouHeader).toBeInTheDocument();
+
+  // expect that loading has disappeared
+  const notLoading = screen.queryByText("loading");
+  expect(notLoading).not.toBeInTheDocument();
+
+  const orderNumber = await screen.findByText(/order number/i);
+  expect(orderNumber).toBeInTheDocument();
+
+  // find and click "new order" button on confirmation page
+  const newOrderButton = screen.getByRole("button", { name: /new order/i });
+  await user.click(newOrderButton);
+
+  // check that scoops and toppings have been reset
+  const scoopsTotal = await screen.findByText("Scoops total: $0.00");
+  expect(scoopsTotal).toBeInTheDocument();
+  const toppingsTotal = screen.getByText("Toppings total: $0.00");
+  expect(toppingsTotal).toBeInTheDocument();
+
+  // unmount the component to trigger cleanup and avoid
+  // "not wrapped in act()" error
+  unmount();
 });
